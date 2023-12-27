e714714cb8b2520ac4f249370599d71c45c0d83f
Rajel Fishman <rajel@Rajels-MacBook-Pro.local>
2023-12-27 15:47:55 +0200

    ffffff

--- AddTwo.java
+++ AddTwo.java
@@ -10,7 +10,7 @@ public class AddTwo {
 		int a = Integer.parseInt(args[0]);
 		int b =  Integer.parseInt(args[1]);
 		int c = a + b; 
-		System.out.println (a+ " + "+b +"  =  "+ c);
+		System.out.println (a  + " + "  + b +  "  =  " + c);
 
 
 	}
