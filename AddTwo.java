df6025cbec6c522e581ba69a6ee5a3c14c29924b
Rajel Fishman <rajel@Rajels-MacBook-Pro.local>
2023-12-28 09:41:18 +0200

    double spacecorrected

--- AddTwo.java
+++ AddTwo.java
@@ -2,15 +2,11 @@
  * Adds two given integers and prints the result in a fancy way.
  */
 public class AddTwo {
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		//3 and  5 are user input and they can be other values and the program will still work
 		int a = Integer.parseInt(args[0]);
 		int b =  Integer.parseInt(args[1]);
 		int c = a + b; 
-		System.out.println (a  + " + "  + b +  "  =  " + c);
+		System.out.println (a  + " + "  + b +  " = " + c);
 
 
 	}
